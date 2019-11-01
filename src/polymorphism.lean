fcc5b1cc56d0412b8c26ad54d6a3bf2b1dcff6d1
sullivan <sullivan.kevinj@gmail.com>
2019-10-31 20:21:04 -0400

    Add namespace

--- src/polymorphism.lean
+++ src/polymorphism.lean
@@ -1,3 +1,5 @@
+namespace my_private
+
 inductive pol_list (α  : Type) : Type
 | nil : pol_list
 | cons : α → pol_list → pol_list
@@ -6,3 +8,5 @@ inductive pol_list (α  : Type) : Type
 
 def pl0 : (pol_list nat) := pol_list.nil ℕ
 def pl1 : (pol_list nat) := pol_list.cons 0 pl0
+
+end my_private
