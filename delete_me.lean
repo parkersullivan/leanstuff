c7845ae75649741cb37c9c368e681879070bdb2a
sullivan <sullivan.kevinj@gmail.com>
2019-10-31 20:16:22 -0400

    first commit

---
+++ delete_me.lean
@@ -0,0 +1,8 @@
+inductive pol_list (α  : Type) : Type
+| nil : pol_list
+| cons : α → pol_list → pol_list
+
+#check pol_list
+
+def pl0 : (pol_list nat) := pol_list.nil ℕ
+def pl1 : (pol_list nat) := pol_list.cons 0 pl0
